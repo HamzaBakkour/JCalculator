e6e3c6a756232c74b2d2c420639f49fa35e3eb02
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-03-05 20:38:16 +0100

    added sum service

--- src/main/java/com/hamza/temp/TempApplication.java
+++ src/main/java/com/hamza/temp/TempApplication.java
@@ -1,13 +1,14 @@
 package com.hamza.temp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+// import main.java.com.hamza.temp;
+
 
 @SpringBootApplication
+@ComponentScan()
 public class TempApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TempApplication.class, args);
 	}
-
 }
